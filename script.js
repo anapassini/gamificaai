6ef9ec50c6dfa82c20f9bc206e614c1b3fd3422f
ANA CLARA PASSINI DE ALMEIDA <57900693807@info.local>
2024-05-23 10:26:03 -0300

    feat: carrossel terminado

--- script.js
+++ script.js
@@ -45,9 +45,39 @@ banner.classList.add(slides[slideAtual])
 function mostrarProximoSlide() {
 
 
-    banner.classList.remove(slide[slideAtual])
+    banner.classList.remove(slides[slideAtual])
 
-    slideAtual++
+    if (slideAtual < 2) {
+        slideAtual++
+    } else {
+
+        slideAtual = 0
+    }
+
+    
+
+    banner.classList.add(slides[slideAtual])
+}
+
+function mostrarSlideAnterior() {
+
+    banner.classList.remove(slides[slideAtual])
+
+    if (slideAtual > 0) {
+        slideAtual--
+    } else {
+
+        slideAtual = 2
+    }
+    
+
+    banner.classList.add(slides[slideAtual])
+}
+
+function selecionarSlide(indiceSlide) {
+    banner.classList.remove(slides[slideAtual])
+
+    slideAtual = indiceSlide
 
     banner.classList.add(slides[slideAtual])
 }
